d5c40e8bc04001db7c1bb8e5a5fe8e9e34b41a59
Timm Kühnel <t_kueh06@uni-muenster.de>
2025-04-04 11:12:39 +0200

    Add Chapters 2.2 to 2.5, Plus Chapter Separation Comments

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,8 @@
+// Chapter 2
+
+// 2.1
+// Layout
+
 /* Layout 1 */
 // pub enum List {
     // Empty, 
@@ -35,15 +40,102 @@ This works because B can never be all 0's, since it contains a non-zero pointer.
 //          also, still does not uniformly allocate on heap
 
 /* Layout 3 */
-struct Node {
-    elem: i32,
-    next: List,
-}
-
-pub enum List {
-    Empty, 
-    More(Box<Node>),
-}
+// struct Node {
+    // elem: i32,
+    // next: List,
+// }
+// 
+// pub enum List {
+    // Empty, 
+    // More(Box<Node>),
+// }
 // Tail of list never allocates extra junk: Check!
 // `enum` is in "delicious" null-pointer-optimized form: Check!
-// 
+// all elements are uniformly allocated on the heap: Check!
+// `List` is a recursive type: Check!
+// `Node` is a recursive type: Check!
+// BUUUUUTTT: Compiler complains -> List is pub, but Node is not, but Node contains the pub List
+
+/* Layout 4 */
+pub struct List {
+    // List is a struct with a single field
+    // --> the size of List is the size of the field
+    // --> Zero Cost Abstraction 
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32, 
+    next: Link,
+}
+
+// 2.2
+// Constructor (for empty list)
+use std::mem;
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    // 2.3 
+    // Push
+    pub fn push(&mut self, elem:i32) {
+        let new_node = Box::new(
+            Node {
+                elem: elem,
+                next: mem::replace(&mut self.head, Link::Empty),
+            }
+        );
+        self.head = Link::More(new_node)
+    }
+
+    // 2.4
+    // Pop
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+// 2.5
+// Testing
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        // check to see if empty lists behave correctly
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // push some more values in just to make sure nothing gets corrupted
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        // check list exhaustiong
+        assert_eq!(list.pop(), None);
+    }
+}
